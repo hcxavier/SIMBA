ccdbb7fed18692ac253b561c18f1cff54533b5d0
Caio Xavier <proxjiec1@gmail.com>
2025-05-01 22:32:36 -0300

    refactor: changes atributes in constructor to String and remove setRegistrationNumber method in the class Student

--- src/br/com/simba/model/entities/Student.java
+++ src/br/com/simba/model/entities/Student.java
@@ -4,16 +4,12 @@ import br.com.simba.model.valueobject.*;
 public class Student extends User{
     private String registrationNumber;
 
-    public Student(Username username, String name, String street, int number , String neighborhood, String city, String stateAbbr, String cep, Email email, Phone phone, Password password, String registrationNumber) {
+    public Student(String username, String name, String street, int number , String neighborhood, String city, String stateAbbr, String cep, String email, String phone, String password, String registrationNumber) {
         super(username, name, street, number, neighborhood, city, stateAbbr, cep, email, phone, password);
         this.registrationNumber = registrationNumber;
     }
     public String getRegistrationNumber() {
         return registrationNumber;
     }
-    public void setRegistrationNumber(String registrationNumber) {
-        this.registrationNumber = registrationNumber;
-    }
-
 }
 
